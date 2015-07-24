4bb6b996a545d356b1028352dae222fc4b568775
oleks-d <olex.dyachuk@gmail.com>
2015-07-24 14:53:40 -0500

    Protractor config and spec were added
    Config file was added.
    Basic spec was added.
    Two additional tests in spec:
    Test that allows to check Select All functionality (left-side checkbox)
    
    "A user can select all items from list."
    Test ability to edit added
    items
      "User can edit items."

---
+++ tests/integration/integration_spec.js
@@ -0,0 +1,48 @@
+describe('Todo list Test',function(){
+
+
+  it("The URL is correct.", function(){
+
+  })
+
+  it("The page title is correct.", function(){
+
+  })
+
+  it("The list of current todo items is correct (blank by default).", function(){
+
+  })
+
+  it("A user can add a todo item.", function(){
+
+  })
+
+
+  it("A user can change views.", function(){
+
+
+  })
+
+  it("A user can mark an item as completed.", function(){
+
+  })
+
+   it("A user can delete an item.", function(){
+
+   })
+
+  it("A user can clear the completed items.", function(){
+
+  })
+
+  // Test that allows to check Select All functionality (left-side checkbox)
+  it("A user can select all items from list.", function(){
+
+  })
+
+  // Test ability to edit added items
+  it("User can edit items.", function(){
+
+  })
+
+});
